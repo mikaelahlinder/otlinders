3802f8893c0a2b30f8066c016202792c80002c2a
Mikael Ahlinder <mikael.ahlinder@aptitud.se>
2018-04-07 15:41:24 +0200

    frame

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="d">
+        <section className="intro">The Otlinders</section>
+        <section className="the-day">rsvp</section>
+        <section className="rsvp">rsvp</section>
+      </div>
+    )
+  }
+}
