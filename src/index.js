1e5b4896852ca52b969f9de7669b9a50387fd834
Mikael Ahlinder <mikael.ahlinder@aptitud.se>
2018-04-26 20:42:20 +0200

    immediate

--- src/index.js
+++ src/index.js
@@ -177,7 +177,7 @@ const App = () => (
         <Image src="deer.png" style={{ height: '30vh' }} />
         <p>
           We love your children! But for this event, we have chosen to limit the
-          number of little people to just family and those travelling from
+          number of little people to just immediate family and those travelling from
           abroad.
         </p>
       </div>
