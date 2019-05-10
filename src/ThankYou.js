79bd97a68778e61f9c3adc8a190a097460a97337
Mikael Ahlinder <mikael.ahlinder@aptitud.se>
2019-05-10 22:01:37 +0200

    Lazy

--- src/ThankYou.js
+++ src/ThankYou.js
@@ -1,5 +1,24 @@
 import React from 'react'
 import Gallery from 'react-photo-gallery'
+import { LazyLoadImage } from 'react-lazy-load-image-component'
 import Images from './data'
 
-export default () => <Gallery photos={Images} direction='column' />
+export default () => (
+  <Gallery
+    photos={Images}
+    direction='column'
+    renderImage={({ photo, margin, top, left }) => (
+      <LazyLoadImage
+        {...photo}
+        effect='opacity'
+        style={{
+          display: 'block',
+          position: 'absolute',
+          left,
+          top,
+          margin
+        }}
+      />
+    )}
+  />
+)
